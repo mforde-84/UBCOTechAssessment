14142e67ab1b0a6727d95a8698aeaf78d14ab6b8
Michael Forde <michael@swipedon.com>
2022-01-23 01:00:16 +1300

    Added react ui

---
+++ ubco.ui/src/App.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import TranslatePage from './features/translate-page/TranslatePage';
+
+class App extends Component {
+  render() {
+    return <TranslatePage />;
+  }
+}
+
+export default App;
